9f5fc7f66b9dc4e6b39c06ad0dab517e12115af8
andrew041994 <andrew041994@gmail.com>
2020-08-30 21:54:21 -0400

    addded button

--- src/components/Test.js
+++ src/components/Test.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 export default class Test extends Component {
   render() {
     return (
-      <div> textInComponent </div>
+      <div> 
+          <button>press for action</button>
+      </div>
     );
   }
 }
